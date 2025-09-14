33104af2a391e792f03f6ff2d2a82b509426fa0d
Abhishekyadav3824 <aradaabhishek@gmail.com>
2025-09-14 10:25:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
+const alltxt=document.querySelectorAll(".strong-cls");
 function highlight() {
+	for(let i=0;i<alltxt.length;i++){
+		alltxt[i].style.color="green"
+	}
     //Write your code here
 
 
@@ -6,6 +10,9 @@ function highlight() {
 
 
 function return_normal() {
+	for(let i=0;i<alltxt.length;i++){
+		alltxt[i].style.color="black"
+	}
     //Write your code here
 
     
